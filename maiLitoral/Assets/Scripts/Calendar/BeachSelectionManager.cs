74c7e51acc61bf3f6f8f812a102c5fb8543c855b
Cosmina Robotin <robotin.co.cosmina@student.utcluj.ro>
2026-04-26 19:08:39 +0300

    Implemented zone selection logic: opening calendar and displaying selected zone name

--- maiLitoral/Assets/Scripts/Calendar/BeachSelectionManager.cs
+++ maiLitoral/Assets/Scripts/Calendar/BeachSelectionManager.cs
@@ -5,9 +5,10 @@ using TMPro;
 // Manages beach selection, shows the calendar and updates the selected beach title.
 public class BeachSelectionManager : MonoBehaviour
 {
-    public GameObject zoneBeachesPanel;
+    public GameObject zoneListPanel;
     public GameObject beachCalendarPanel;
     public TMP_Text selectedBeachTitle;
+
 
     // Called when a beach/zone panel is pressed.
     public void SelectBeach(string beachName)
@@ -16,7 +17,16 @@ public class BeachSelectionManager : MonoBehaviour
 
         selectedBeachTitle.text = beachName;
 
-        zoneBeachesPanel.SetActive(false);
+        zoneListPanel.SetActive(false);
         beachCalendarPanel.SetActive(true);
     }
+
+    public void BackToZoneList()
+    {
+        Debug.Log("SEARCH BAR CLICKED - BACK TO LIST");
+
+        beachCalendarPanel.SetActive(false);
+        zoneListPanel.SetActive(true);
+    }
 }
+
